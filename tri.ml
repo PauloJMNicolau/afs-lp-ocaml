0c29bc772a25f28543c5cb7f998d1e6f1e04fbe7
Paulo Nicolau <paulojmnicolau227@gmail.com>
2021-03-18 23:01:12 +0000

    add 4 (triangulo)

---
+++ tri.ml
@@ -0,0 +1,10 @@
+let tri a b c = (
+  if (a < (b + c) || b > (a+c)) || c > (a+b) then
+    0
+  else if ((a = b) && (a = c)) && b = c then
+    1
+  else if (a = b || a = c) || b = c then
+    2
+  else
+    3
+);;
