346bd6eba003b79ab67b135db5c34fc9bb294e1a
Paulo Nicolau <paulojmnicolau227@gmail.com>
2021-03-19 17:18:48 +0000

    add listas upgrade

--- listas.ml
+++ listas.ml
@@ -16,11 +16,22 @@ let rec sum lista =(
 
 (*Concatenar Listas*)
 let rec concat lista =(
-  match lista with
-  |[] -> ""
-  |h::t -> h ^ (concat t)
+  match lista with        (*Verifica se a lista: *)
+  |[] -> ""               (*está vazia - retorna string vazia*)
+  |h::t -> h ^ (concat t) (*tem elementos -concatena valor da cabeça com valor retornado da execuçãp recursiva com a cauda da lista*)
 );;
 
-(len [1;1;2;3;4;1]);;
-(sum [1;1;2;3;4;1]);;
-(concat ["ola";"";"ole"]);;
+(len [1;1;2;3;4;1]);; (*Retorna 6*)
+(sum [1;1;2;3;4;1]);; (*Retorna 12*)
+(concat ["ola";"";"ole"]);; (*Retorna "olaole"*)
+
+(*Exercicio 9*)
+(*Retorna uma lista com os valores sucessivos dos valores da lista de entrada*)
+let rec succAll lista =(
+  match lista with              (*Verifica se a lista: *)
+  |[] -> []                     (*está vazia - retorna string vazia*)
+  | h::t -> (h+1)::(succAll t)  (*tem elementos - incrementa valor da cabeça da lista e adiciona ao valor retornado da execução recursiva*)
+);;
+
+(succAll[]);;         (*Retorna lista vazia*)
+(succAll[3;6;1;0;-4]);; (*Retorna [4,7,2;1;-3]*)
